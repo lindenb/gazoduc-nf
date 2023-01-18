8b25ea5ca92e5445b04e34ce7f1e229ceeb4e28b
lindenb <plindenbaum@yahoo.fr>
2023-01-19 00:54:27 +0100

    add javadoc

---
+++ src/main/java/gazoduc/package-info.java
@@ -0,0 +1,9 @@
+/**
+<h1>A helper package for <a>nextflow</a></h1>
+<pre>def gazoduc = gazoduc.Gazoduc.getInstance(params);</pre>
+
+@since 2023-01-19
+@author Pierre Lindenbaum
+@version 0.1
+*/
+package gazoduc;
